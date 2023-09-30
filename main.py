8a9af52f02bb95f7b7feca484cb3ce90e356c185
dv <dv@shamit.org>
2023-09-30 19:06:25 +0530

    Static hai

---
+++ main.py
@@ -0,0 +1,28 @@
+import pygame
+import json
+
+pygame.init()
+surface = pygame.display.set_mode((500, 625),pygame.RESIZABLE)
+
+color_list = []
+
+f = open('colors.json')
+data = json.load(f)
+
+for i in data['colors']:
+    print(i['color'])
+    color_list.append(i['color'])   
+
+running =  True
+while running:
+
+    x,y = surface.get_size()
+
+    for i in range(0,5):
+        pygame.draw.rect(surface, color_list[i], pygame.Rect(0, i*(y/5),(x) , y/5))
+
+    pygame.display.flip()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
